5bd12c8eb5bf5262368192fb3eeefcf3a3d720eb
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2014-10-20 15:12:12 -0700

    main

---
+++ CicleCalculations.java
@@ -0,0 +1,15 @@
+public class CircleCalculations
+{
+    public static double calculateArea(int radius)
+	{
+		return Math.PI*radius*radius;
+	}
+	
+	public static double calculateCircumference(int radius)
+	{
+		return Math.PI*radius*2.0;
+	}
+}
+
+//---------------------------
+
